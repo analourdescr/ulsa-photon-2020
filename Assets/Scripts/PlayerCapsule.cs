48a817d55a6f61d690a58d1579623d30c7f610c5
Ana Lourdes Cota Rascón <ana_lourdes98@hotmail.com>
2020-11-13 11:29:17 -0700

    hasta el lobby

---
+++ Assets/Scripts/PlayerCapsule.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerCapsule : MonoBehaviour
+{
+    [SerializeField]
+    float moveSpeed;
+    
+
+    void Update()
+    {
+        transform.Translate(Axis.normalized.magnitude * Vector3.forward * moveSpeed * Time.deltaTime);
+
+        if(Axis != Vector3.zero)
+        {
+            transform.rotation = Quaternion.LookRotation(Axis);
+        }
+        
+
+    }
+}
